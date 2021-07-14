a0776960ca4c38488e37091a59fc259677a7e693
Andrew Massey <acmassey@ncsu.edu>
2021-07-14 16:54:40 -0400

    Create user class and initialize open mic

--- lib/joke.rb
+++ lib/joke.rb
@@ -3,10 +3,8 @@ class Joke
   attr_reader :index, :setup, :punchline
 
   def initialize(index, setup, punchline)
-
     @index = index
     @setup = setup
     @punchline = punchline
-
   end
 end
---
+++ lib/user.rb
@@ -0,0 +1,24 @@
+class User
+  attr_accessor :jokes
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @jokes = []
+  end
+
+  def learn(joke)
+    @jokes << joke
+  end
+
+  def tell(other_user, joke)
+    other_user.jokes << joke
+  end
+
+  def joke_by_id(joke_id)
+    joke = @jokes.find do |joke|
+      joke.index == joke_id
+    end
+  end
+end
